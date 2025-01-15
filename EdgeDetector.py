962cea69356261a5a89132cef1644f54b657fb95
Alex Chen <1513214585@qq.com>
2025-01-15 15:35:11 -0500

    After testing with the board, board.GP15 cannot appear twice.

--- EdgeDetector.py
+++ EdgeDetector.py
@@ -12,12 +12,6 @@ led.direction = digitalio.Direction.OUTPUT
 # initialize the value of the LED to off
 led.value = False
 
-# configure the button to GP15
-button = digitalio.DigitalInOut(board.GP15)
-button.direction = digitalio.Direction.INPUT
-# set the pull up resistor
-button.pull = digitalio.Pull.UP
-
 # configure the edge counter
 # following example from https://docs.circuitpython.org/en/latest/shared-bindings/countio/index.html#countio.Edge
 edgeCounter = countio.Counter(board.GP15, edge=countio.Edge.FALL)
